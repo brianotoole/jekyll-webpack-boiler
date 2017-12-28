d0314c56d5d4b20c91384d5e6c6846af3a7331ac
Brian O'Toole <brian@sparxoo.com>
2017-12-28 18:28:11 -0500

    added header + nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
 // MAIN
 
-console.log('testing main.js');
+// Mobile nav
+$(document).ready(function() {
+  var trigger = $('#js-nav-toggle');
+  var isClosed = false;
+  trigger.click(function() {
+    $('#js-nav-mobile').toggleClass('nav-open');
+    $('body').toggleClass('nav-open');
+    $('#js-nav-toggle').toggleClass('active');
+  });
+});
